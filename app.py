844da45626038757df7808e6cc07d13cf0cff0f1
milanitommaso <milani.tommaso2004@gmail.com>
2025-02-12 15:09:19 +0100

    api routes

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/api/get-averages/<day>")
+def get_averages(day):
+    return "data"
+
+
+@app.route("/api/start-listen/<day>/<singer_id>")
+def start_listen(day, singer_id):
+    return "started"
+
+
+@app.route("/api/stop-listen")
+def stop_listen():
+    return "stopped"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=8000)
